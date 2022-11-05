c5b2ee23b5053088b69e43ffee84afd0ed341f1f
ozodsilver <ozodsilver@gmail.com>
2022-11-05 15:19:13 +0500

    tayyor emass

--- src/components/Goods/FastFoods.vue
+++ src/components/Goods/FastFoods.vue
@@ -1,128 +1,160 @@
 <template>
-    <div>
+  <div id="fast_foods">
+    <div
+      class="w-100 d-flex justify-content-center align-items-center"
+      style="height: 100vh"
+      v-if="loade"
+    >
+      <div
+        class="spinner-grow text-white"
+        style="width: 3rem; height: 3rem"
+        role="status"
+      >
+        <span class="visually-hidden"></span>
+      </div>
+    </div>
 
+    <div class="row">
+      <div class="col-12" style="min-height: 100vh">
+        <div v-for="foods in FastFoods" :key="foods._id">
+          <div class="card mb-3 shadow-lg">
+            <div class="row g-0">
+              <div class="col-md-5">
+                <img
+                  :src="`https://dilkhush-fayz.herokuapp.com${foods.image}`"
+                  alt="Maxsulot Rasmi Mavjud emas"
+                  class="img-fluid w-100 rounded-start rounded-lg"
+                />
+              </div>
+              <div class="col-md-7">
+                <div class="card-body">
+                  <h3 class="card-title">{{ foods.name }}</h3>
+                  <p class="card-text">
+                    {{ foods.description }}
+                  </p>
 
-        <div class="w-100  d-flex justify-content-center align-items-center" style="height: 100vh;" v-if="loade">
-    <div class="spinner-grow  text-white" style="width: 3rem; height: 3rem;" role="status">
-  <span class="visually-hidden"></span>
-</div>
-</div>
+                  <p class="card-text d-flex align-items-center">
+                    <b class="badge bg-warning">Narx</b>
+                    <b class="ml-2">{{ foods.price }}</b>
+                  </p>
+                  <p class="card-text">
+                    <b class="badge bg-warning">Details</b>
+                    {{ foods.details }}
+                  </p>
+                  <p class="card-text">
+                    <b class="badge bg-warning">Chegirma</b>
+                    {{ foods.chegirma }}
+                  </p>
+                  <p class="card-text">
+                    <small class="text-muted"
+                      >oxirgi yangilanish {{ foods.updatedAt }}</small
+                    >
+                  </p>
 
-
-        <div class="row">
-  <div class="col-12" style="min-height: 100vh;">
- 
-  <div v-for="foods in FastFoods" :key="foods._id">
-                <div class="card mb-3 shadow-lg " >
-                  <div class="row g-0">
-                    <div class="col-md-5 ">
-                      <img
-                        :src="`https://dilkhush-fayz.herokuapp.com${foods.image}`"
-                        alt="Maxsulot Rasmi Mavjud emas"
-                        class="img-fluid w-100 rounded-start rounded-lg"
-                      />
-                    </div>
-                    <div class="col-md-7">
-                      <div class="card-body" >
-                        <h3 class="card-title">{{ foods.name }}</h3>
-                        <p class="card-text">
-                          {{ foods.description }}
-                        </p>
-
-                        <p class="card-text d-flex align-items-center">
-                          <b class="badge bg-warning">Narx</b>
-                          <b class="ml-2">{{ foods.price }}</b>
-                        </p>
-                        <p class="card-text">
-                          <b class="badge bg-warning">Details</b>
-                          {{ foods.details }}
-                        </p>
-                        <p class="card-text">
-                          <b class="badge bg-warning">Chegirma</b>
-                          {{ foods.chegirma }}
-                        </p>
-                        <p class="card-text">
-                          <small class="text-muted"
-                            >oxirgi yangilanish {{ foods.updatedAt }}</small
-                          >
-                       
-                        
-                        </p>
-
-                       <div class="d-flex justify-content-end gap-2">
-                        <button class=" btn-danger border-0 p-2 rounded" @click="DeleteProduct(foods._id)">O'chirish</button>
-                       </div>
-                      </div>
-                    </div>
+                  <div class="d-flex justify-content-end gap-2">
+                    <button
+                      class="btn-warning border-0 p-2 rounded"
+                      @click="Isactive(foods._id)"
+                    >
+                      Inactive
+                    </button>
+                    <button
+                      class="btn-danger border-0 p-2 rounded"
+                      @click="DeleteProduct(foods._id)"
+                    >
+                      O'chirish
+                    </button>
                   </div>
                 </div>
               </div>
-</div>
-
-</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
-    export default {
-        data(){
-            return {
-                FastFoods:[],
-                loade: true,
-                
-        }
-    },
+export default {
+  data() {
+    return {
+      FastFoods: [],
+      loade: true,
+    };
+  },
 
   async mounted() {
- 
-  await axios
-    .get(
-      `https://dilkhush-fayz.herokuapp.com/api/product/byCategory?category=fast_foods`,
+    await axios
+      .get(
+        `https://dilkhush-fayz.herokuapp.com/api/product/byCategory?category=fast_foods`,
 
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `${localStorage.getItem("jwt")}`,
-        },
-      }
-    )
-    .then((res) => {
-      console.log(res)
-      this.loade = false;
-      res.data.data.forEach((el) => {
-     
-        this.FastFoods.push(el);
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("jwt")}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        this.loade = false;
+        res.data.data.forEach((el) => {
+          this.FastFoods.push(el);
+        });
       });
-    });
-},
+  },
 
-methods:{
- async DeleteProduct(id){
+  methods: {
+    async DeleteProduct(id) {
+      console.log(id);
+      await axios.delete(
+        `https://dilkhush-fayz.herokuapp.com/api/product/${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${localStorage.getItem("jwt")}`,
+          },
+        }
+      );
 
-  console.log(id)
-await axios.delete(`https://dilkhush-fayz.herokuapp.com/api/product/${id}`, 
-{
-  headers: {
-          "Content-Type": "application/json",
-          Authorization: `${localStorage.getItem("jwt")}`,
-        },
-})
+      setTimeout(() => {
+        this.$router.replace("/Home");
+      }, 10);
 
-setTimeout(()=>{
-this.$router.replace('/Home')
-     },10)
+      setTimeout(() => {
+        this.$router.replace("/profile");
+      }, 500);
+    },
 
-     setTimeout(()=>{
-this.$router.replace('/profile')
-     },500)
-
-  }
-}
-}
+    async Isactive(id2) {
+      await axios
+        .put(
+          `https://dilkhush-fayz.herokuapp.com/api/product/isExist/${id2}`,
+          {
+            isExist: false,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `${localStorage.getItem("jwt")}`,
+            },
+          }
+        )
+        .then((el) => {
+          console.log(el);
+          if (el.data.data.isExist == false) {
+            alert("Mahsulot deactive qilindi");
+          }
+        });
+    },
+  },
+};
 </script>
 
 <style scoped>
+
 
 </style>
--- src/components/Tables.vue
+++ src/components/Tables.vue
@@ -1,82 +1,140 @@
-<template>
-  <section id="tables">
-    <mdb-row>
-      <mdb-col md="12">
-        <mdb-card cascade narrow class="mt-5">
-          <mdb-view class="gradient-card-header blue darken-2">
-            <h4 class="h4-responsive text-white">Basic tables</h4>
-          </mdb-view>
-          <mdb-card-body>
-            <h3 class="mt-5 text-left"><strong>Basic examples</strong></h3>
-            <p>Using the most basic table markup, here’s how .table-based tables look in Bootstrap. All table styles are inherited in Bootstrap 4, meaning any nested tables will be styled in the same manner as the parent.</p>
-            <mdb-tbl>
-              <thead>
-                <tr><th>#</th><th>First Name</th><th>Last Name</th><th>Username</th></tr>
-              </thead><tbody>
-                <tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr><tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td><td>@fat</td></tr><tr><th scope="row">3</th><td>Larry</td><td>the Bird</td><td>@twitter</td></tr>
-              </tbody>
-            </mdb-tbl>
-            <h3 class="mt-5 text-left"><strong>Table head options</strong></h3>
-            <p>To change a background-color of thead (or any other element) use our color classes. If you are going to use a dark background you should also consider white text (to provide a proper contrast) by adding .text-white class. </p>
-            <mdb-tbl>
-              <thead class="blue-grey lighten-4">
-                <tr><th>#</th><th>First Name</th><th>Last Name</th><th>Username</th></tr>
-              </thead>
-              <tbody>
-                <tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr><tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td><td>@fat</td></tr><tr><th scope="row">3</th><td>Larry</td><td>the Bird</td><td>@twitter</td></tr>
-              </tbody>
-            </mdb-tbl>
-            <mdb-tbl>
-              <thead class="mdb-color darken-3">
-                <tr class="text-white"><th>#</th><th>First Name</th><th>Last Name</th><th>Username</th></tr>
-              </thead>
-              <tbody>
-                <tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr><tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td><td>@fat</td></tr><tr><th scope="row">3</th><td>Larry</td><td>the Bird</td><td>@twitter</td></tr>
-              </tbody>
-            </mdb-tbl>
-            <h3 class="mt-5 text-left"><strong>Striped rows:</strong></h3>
-            <table class="table table-striped">
-              <thead>
-                <tr><th>#</th><th>First Name</th><th>Last Name</th><th>Username</th></tr>
-              </thead>
-              <tbody>
-                <tr><th scope="row">1</th><td>Mark</td><td>Otto</td><td>@mdo</td></tr><tr><th scope="row">2</th><td>Jacob</td><td>Thornton</td><td>@fat</td></tr><tr><th scope="row">3</th><td>Larry</td><td>the Bird</td><td>@twitter</td></tr>
-              </tbody>
-            </table>
-          </mdb-card-body>
-        </mdb-card>
-      </mdb-col>
-    </mdb-row>
-  </section>
+<template >
+    <div id="active" class="p-4">
+      <div class="container">
+
+
+<div class="w-100  d-flex justify-content-center align-items-center" style="height: 100vh;" v-if="loade">
+<div class="spinner-grow  text-white" style="width: 3rem; height: 3rem;" role="status">
+<span class="visually-hidden"></span>
+</div>
+</div>
+
+
+<div class="row">
+<div class="col-12" style="min-height: 100vh;">
+
+<div v-for="foods in FastFoods" :key="foods._id">
+        <div class="card mb-3 shadow-lg " >
+          <div class="row g-0">
+            <div class="col-md-5 ">
+              <img
+                :src="`https://dilkhush-fayz.herokuapp.com${foods.image}`"
+                alt="Maxsulot Rasmi Mavjud emas"
+                class="img-fluid w-100 rounded-start rounded-lg"
+              />
+            </div>
+            <div class="col-md-7">
+              <div class="card-body" >
+                <h3 class="card-title">{{ foods.name }}</h3>
+                <p class="card-text">
+                  {{ foods.description }}
+                </p>
+
+                <p class="card-text d-flex align-items-center">
+                  <b class="badge bg-warning">Narx</b>
+                  <b class="ml-2">{{ foods.price }}</b>
+                </p>
+                <p class="card-text">
+                  <b class="badge bg-warning">Details</b>
+                  {{ foods.details }}
+                </p>
+                <p class="card-text">
+                  <b class="badge bg-warning">Chegirma</b>
+                  {{ foods.chegirma }}
+                </p>
+                <p class="card-text">
+                  <small class="text-muted"
+                    >oxirgi yangilanish {{ foods.updatedAt }}</small
+                  >
+               
+                
+                </p>
+
+               <div class="d-flex justify-content-end gap-2">
+                <button class=" btn-success border-0 p-2 rounded" @click="Isactive(foods._id)">Active</button>
+        
+             
+               </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+
+</div>
+</div>
+    </div>
 </template>
 
 <script>
-import { mdbRow, mdbCol, mdbCard, mdbView, mdbCardBody, mdbTbl } from 'mdbvue'
+import axios from 'axios';
 
-export default {
-  name: 'Tables',
-  components: {
-    mdbRow,
-    mdbCol,
-    mdbCard,
-    mdbView,
-    mdbCardBody,
-    mdbTbl
-  },
-  data () {
-    return {}
-  }
+    export default {
+        data(){
+            return {
+                FastFoods:[],
+                loade: true,
+                
+        }
+    },
+
+  async mounted() {
+ 
+  await axios
+    .get(
+      `https://dilkhush-fayz.herokuapp.com/api/product/byCategory?category=fast_foods`,
+
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${localStorage.getItem("jwt")}`,
+        },
+      }
+    )
+    .then((res) => {
+      console.log(res)
+      this.loade = false;
+      res.data.data.forEach((el) => {
+     
+        this.FastFoods.push(el);
+      });
+    });
+},
+
+methods:{
+  async Isactive(id) {
+      await axios
+        .put(
+          `https://dilkhush-fayz.herokuapp.com/api/product/isExist/${id}`,
+          {
+            isExist: true,
+          },
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `${localStorage.getItem("jwt")}`,
+            },
+          }
+        )
+        .then((el) => {
+          console.log(el);
+          if (el.data.data.isExist == false) {
+            alert("Mahsulot deactive qilindi");
+          }
+        });
+    },
+}
+
+
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.card.card-cascade .view.gradient-card-header {
-  padding: 1rem 1rem;
-  text-align: center;
-}
-.card.card-cascade h3,
-.card.card-cascade h4 {
-  margin-bottom: 0;
+#active{
+  background: url('https://phonoteka.org/uploads/posts/2022-02/1644356122_31-phonoteka-org-p-fon-dlya-blyud-33.jpg');
+  background-position: center;
+  background-attachment: fixed;
+
 }
 </style>
